9e2485e2bec71f4b153c9bf67e106fc4b4c7427a
sucoso <65839195+nitsuboy@users.noreply.github.com>
2022-08-17 14:21:27 -0300

    Segurança

--- src/main/java/mandacaru/controller/ImovelController.java
+++ src/main/java/mandacaru/controller/ImovelController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,6 +36,7 @@ public class ImovelController {
     }
     
     @PostMapping(path = "/usuarios/{id}/imoveis")
+    @PreAuthorize("isAuthenticated()")  
     public void save(@PathVariable("id") int usuario_id,@RequestBody Imovel imovel) {
         service.save(usuario_id, imovel);
     }
