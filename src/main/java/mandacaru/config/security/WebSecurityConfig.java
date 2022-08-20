b8cc5f1fef4245ac2f72938e93d376ee95743e7e
sucoso <65839195+nitsuboy@users.noreply.github.com>
2022-08-20 11:30:13 -0300

    Segurança
    Outh n ta funcionando

--- src/main/java/mandacaru/config/security/WebSecurityConfig.java
+++ src/main/java/mandacaru/config/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package mandacaru.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,6 +18,12 @@ import org.springframework.stereotype.Component;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+	    return super.authenticationManagerBean();
+	}
 	
 	@Autowired
     UserDetailsServiceImpl userDetailsServiceImpl;
