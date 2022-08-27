4eda89bc2903ef163403b6e7822f49ada43fca48
Jonatasmvb <jonatasmvb@gmail.com>
2022-08-27 15:24:11 -0300

    Swagger
    Algumas atualizações

--- src/main/java/mandacaru/controller/ImovelController.java
+++ src/main/java/mandacaru/controller/ImovelController.java
@@ -16,10 +16,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import mandacaru.Pdt;
 import mandacaru.model.Imovel;
-import mandacaru.service.ImovelService;
-import mandacaru.Pdt; 
+import mandacaru.service.ImovelService; 
 
+@Tag(name = "Imovel")
 @RestController
 @RequestMapping(path = "/api") 
 public class ImovelController {
@@ -27,16 +30,19 @@ public class ImovelController {
     @Autowired
     ImovelService service;
     
+    @Operation(summary = "Find all properties of a user ")
     @GetMapping(path = "/usuarios/{id}/imoveis")
     public ResponseEntity<List<Imovel>> findallofuser(@PathVariable(value = "id") int id) {
         return new ResponseEntity<List<Imovel>>(service.findAllOfUser(id), HttpStatus.OK);
     }
     
+    @Operation(summary = "Find all properties")
     @GetMapping(path = "/imoveis")
     public ResponseEntity<List<Imovel>> findall() {
         return new ResponseEntity<List<Imovel>>(service.findAll(), HttpStatus.OK);
     }
- 
+    
+    @Operation(summary = "Find property by ID")
     @GetMapping(path = "/imoveis/{id}")
     public ResponseEntity<Imovel> find(@PathVariable("id") int id) {
     	Imovel imovel = service.find(id);
@@ -48,14 +54,14 @@ public class ImovelController {
 		}
     }
     
-    //
-    
+    @Operation(summary = "Save the property")
     @PostMapping(path = "/usuarios/{id}/imoveis")
     public void save(@PathVariable("id") int usuario_id,@RequestBody Imovel imovel) throws ParseException, IOException, InterruptedException {
     		
         service.save(usuario_id, imovel);
     }
     
+    @Operation(summary = "teste")
     @PostMapping(path = "/teste")
     public String teste() throws ParseException {
     	Pdt t = new Pdt();
@@ -63,15 +69,13 @@ public class ImovelController {
         return "show";
     }
  
-    //
-    
+    @Operation(summary = "Update a property")
     @PutMapping(path = "/imoveis/{id}")
     public void update(@PathVariable("id") int id, @RequestBody Imovel imovel) {
         service.update(id, imovel);
     }
  
-    //
-    
+    @Operation(summary = "Delete a property")
     @DeleteMapping(path = "/imoveis/{id}")
     public void delete(@PathVariable("id") int id) {
         service.delete(id);
