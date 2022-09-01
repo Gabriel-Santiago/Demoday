9d30797c35981b91087fb6274a8636196abe7600
Gabriel <gabrielsrmj@alu.ufc.br>
2022-09-01 15:56:16 -0300

    Teste

--- src/test/java/mandacaru/Atividade_2/controller/ImovelControllerTest.java
+++ src/test/java/mandacaru/Atividade_2/controller/ImovelControllerTest.java
@@ -8,9 +8,9 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.apache.tomcat.util.json.ParseException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -20,23 +20,32 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import mandacaru.controller.ImovelController;
+import mandacaru.model.Imagem;
 import mandacaru.model.Imovel;
 import mandacaru.model.Usuario;
 import mandacaru.service.ImovelService;
 
 public class ImovelControllerTest {
 
+	// Imovel
 	private static final int ID = 1;
 	private static final String TITULO = "Mansão UFC";
 	private static final String ENDERECO = "Av. Ceará";
-	private static final String STATUS = "Pendente";
 	private static final double METROS_QUADRADOS_DE_TERRENO = 306.90;
 	private static final int QUANTIDADE_DE_QUARTOS = 6;
 	private static final int QUANTIDADE_DE_BANHEIROS = 5;
-	private static final double PRECO = 720963.81;
 	private static final int QUANTIDADE_DE_VAGAS_DE_GARAGEM = 8;
-	private static final String PROCESSO = "6229bb06-dabc-420d-ac72-d2cf67c775f9";
-	private Usuario USUARIO;
+	private static final double PRECO = 720963.81;
+	private static final String STATUS = "Pendente";
+	private static final String PROCESSO = "sjvapoofa7v7ev498v48r4ve98f4";
+	private Usuario usuario;
+	
+	
+	
+	// Imagens
+	private static final int ID_1 = 1;
+	private static final String NOME = "Teste";
+	private static final String TIPO = "PNG";
 	
 	@InjectMocks
 	private ImovelController controller;
@@ -46,25 +55,50 @@ public class ImovelControllerTest {
 	
 	private Imovel imovel;
 	
+	private List<Imagem> listImagem;
+	
 	private void start() {
-		Imovel imovel = new Imovel();
-		imovel.setId(ID);
-		imovel.setTitulo(TITULO);
-		imovel.setEndereco(ENDERECO);
-		imovel.setStatus(STATUS);
-		imovel.setMetros_quadrados_de_terreno(METROS_QUADRADOS_DE_TERRENO);
-		imovel.setQuantidade_de_quartos(QUANTIDADE_DE_QUARTOS);
-		imovel.setQuantidade_de_banheiros(QUANTIDADE_DE_BANHEIROS);
-		imovel.setPreco(PRECO);
-		imovel.setQuantidade_de_vagas_de_garagem(QUANTIDADE_DE_VAGAS_DE_GARAGEM);
-		imovel.setProcesso(PROCESSO);
-		imovel.setUsuario(USUARIO);
+		Imagem imagem = new Imagem();
+		imagem.setId(ID_1);
+		imagem.setNome(NOME);
+		imagem.setTipo(TIPO);
+		imagem.setFoto(null);
+		
+		listImagem = new ArrayList<Imagem>();
+		listImagem.add(imagem);
+		
+		imovel = new Imovel(ID, TITULO, ENDERECO, METROS_QUADRADOS_DE_TERRENO, QUANTIDADE_DE_QUARTOS, 
+				QUANTIDADE_DE_BANHEIROS, QUANTIDADE_DE_VAGAS_DE_GARAGEM, PRECO,STATUS, PROCESSO,
+				 listImagem, usuario);		
 	}
 	
 	@BeforeEach
 	public void setUp() {
 		MockitoAnnotations.openMocks(this);
 		start();
+	}
+	
+	@Test
+	public void whenFindByIdThenReturnSuccess() {
+		when(service.find(anyInt())).thenReturn(imovel);
+
+		ResponseEntity<Imovel> response = controller.find(ID);
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(ResponseEntity.class, response.getClass());
+		assertEquals(Imovel.class, response.getBody().getClass());
+		assertEquals(ID, response.getBody().getId());
+		assertEquals(TITULO, response.getBody().getTitulo());
+		assertEquals(ENDERECO, response.getBody().getEndereco());
+		assertEquals(METROS_QUADRADOS_DE_TERRENO, response.getBody().getMetros_quadrados_de_terreno());
+		assertEquals(QUANTIDADE_DE_QUARTOS, response.getBody().getQuantidade_de_quartos());
+		assertEquals(QUANTIDADE_DE_BANHEIROS, response.getBody().getQuantidade_de_banheiros());
+		assertEquals(QUANTIDADE_DE_VAGAS_DE_GARAGEM, response.getBody().getQuantidade_de_vagas_de_garagem());
+		assertEquals(PRECO, response.getBody().getPreco());
+		assertEquals(STATUS, response.getBody().getStatus());
+		assertEquals(PROCESSO, response.getBody().getProcesso());
+		assertEquals(listImagem, response.getBody().getImagens());
+		assertEquals(usuario, response.getBody().getUsuario());
 	}
 	
 	@Test
@@ -79,20 +113,80 @@ public class ImovelControllerTest {
 	}
 	
 	@Test
-	public void whenCreateThenReturnSuccess() throws ParseException, IOException, InterruptedException {
-		doNothing().when(service).save(USUARIO.getId(), imovel);
-
-		controller.save(USUARIO.getId(), imovel);
-
-		verify(service).save(USUARIO.getId(), imovel);
+	public void whenFindAllThenReturnSuccess() {
+		when(service.findAll()).thenReturn(List.of(imovel));
+		
+		ResponseEntity<List<Imovel>> response = controller.findall();
+		
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(ResponseEntity.class, response.getClass());
+		assertEquals(ID, response.getBody().get(0).getId());
+		assertEquals(TITULO, response.getBody().get(0).getTitulo());
+		assertEquals(ENDERECO, response.getBody().get(0).getEndereco());
+		assertEquals(METROS_QUADRADOS_DE_TERRENO, response.getBody().get(0).getMetros_quadrados_de_terreno());
+		assertEquals(QUANTIDADE_DE_QUARTOS, response.getBody().get(0).getQuantidade_de_quartos());
+		assertEquals(QUANTIDADE_DE_BANHEIROS, response.getBody().get(0).getQuantidade_de_banheiros());
+		assertEquals(QUANTIDADE_DE_VAGAS_DE_GARAGEM, response.getBody().get(0).getQuantidade_de_vagas_de_garagem());
+		assertEquals(PRECO, response.getBody().get(0).getPreco());
+		assertEquals(STATUS, response.getBody().get(0).getStatus());
+		assertEquals(PROCESSO, response.getBody().get(0).getProcesso());
+		assertEquals(listImagem, response.getBody().get(0).getImagens());
+		assertEquals(usuario, response.getBody().get(0).getUsuario());
 	}
-
+	
 	@Test
-	public void whenDeleteThenReturnSuccess() { 
+	public void whenFindAllOfUserThenReturnSuccess() {
+		when(service.findAllOfUser(ID)).thenReturn(List.of(imovel));
+		
+		ResponseEntity<List<Imovel>> response = controller.findallofuser(ID);
+		
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(ResponseEntity.class, response.getClass());
+		assertEquals(ID, response.getBody().get(0).getId());
+		assertEquals(TITULO, response.getBody().get(0).getTitulo());
+		assertEquals(ENDERECO, response.getBody().get(0).getEndereco());
+		assertEquals(METROS_QUADRADOS_DE_TERRENO, response.getBody().get(0).getMetros_quadrados_de_terreno());
+		assertEquals(QUANTIDADE_DE_QUARTOS, response.getBody().get(0).getQuantidade_de_quartos());
+		assertEquals(QUANTIDADE_DE_BANHEIROS, response.getBody().get(0).getQuantidade_de_banheiros());
+		assertEquals(QUANTIDADE_DE_VAGAS_DE_GARAGEM, response.getBody().get(0).getQuantidade_de_vagas_de_garagem());
+		assertEquals(PRECO, response.getBody().get(0).getPreco());
+		assertEquals(STATUS, response.getBody().get(0).getStatus());
+		assertEquals(PROCESSO, response.getBody().get(0).getProcesso());
+		assertEquals(listImagem, response.getBody().get(0).getImagens());
+		assertEquals(usuario, response.getBody().get(0).getUsuario());
+	}
+	
+	@Test
+	public void whenFindAllDoneThenReturnSuccess() {
+		when(service.findAllDone()).thenReturn(List.of(imovel));
+		
+		ResponseEntity<List<Imovel>> response = controller.findalldone();
+		
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(ResponseEntity.class, response.getClass());
+		assertEquals(ID, response.getBody().get(0).getId());
+		assertEquals(TITULO, response.getBody().get(0).getTitulo());
+		assertEquals(ENDERECO, response.getBody().get(0).getEndereco());
+		assertEquals(METROS_QUADRADOS_DE_TERRENO, response.getBody().get(0).getMetros_quadrados_de_terreno());
+		assertEquals(QUANTIDADE_DE_QUARTOS, response.getBody().get(0).getQuantidade_de_quartos());
+		assertEquals(QUANTIDADE_DE_BANHEIROS, response.getBody().get(0).getQuantidade_de_banheiros());
+		assertEquals(QUANTIDADE_DE_VAGAS_DE_GARAGEM, response.getBody().get(0).getQuantidade_de_vagas_de_garagem());
+		assertEquals(PRECO, response.getBody().get(0).getPreco());
+		assertEquals(STATUS, response.getBody().get(0).getStatus());
+		assertEquals(PROCESSO, response.getBody().get(0).getProcesso());
+		assertEquals(listImagem, response.getBody().get(0).getImagens());
+		assertEquals(usuario, response.getBody().get(0).getUsuario());
+	}
+	
+	@Test
+	public void whenDeleteThenReturnSuccess() {
 		doNothing().when(service).delete(ID);
 
 		controller.delete(ID);
 
 		verify(service).delete(ID);
-	}	
+	}		
 }
